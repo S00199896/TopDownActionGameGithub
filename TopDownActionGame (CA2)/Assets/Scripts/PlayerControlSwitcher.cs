0d354cecb636a12fa23e5efd1c09942646628637
agallagher000 <60218400+agallagher000@users.noreply.github.com>
2020-03-02 10:54:02 +0000

    update1

---
+++ TopDownActionGame (CA2)/Assets/Scripts/PlayerControlSwitcher.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlSwitcher : MonoBehaviour
+{
+
+    bool canEnterVehicle;
+    bool isInVehicle;
+
+    GameObject currentVehicle;
+    PlayerOnFootMovement playerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerOnFootMovement>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("EnterTrigger") && !isInVehicle)
+        {
+            canEnterVehicle = true;
+            currentVehicle = collision.transform.parent.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("EnterTrigger") && isInVehicle)
+        {
+            canEnterVehicle = false;
+            currentVehicle = null; //null means there is nothing stored in this variable, trying to access it will result in an error
+        }
+    }
+
+    private void Update()
+    {
+        if(canEnterVehicle && !isInVehicle)
+        {
+            //enter car
+            if (Input.GetKeyDown(KeyCode.E)) //GetKeyDown only activates once when the button is pressed once
+                enterCar();
+        }
+        else if (isInVehicle)
+        {
+            //exit car
+            if (Input.GetKeyDown(KeyCode.E))
+                exitCar();
+        }
+    }
+
+    void enterCar()
+    {
+        //update player
+        playerMovement.enabled = false;
+        playerMovement.gameObject.GetComponent<Collider2D>().enabled = false; //turns off collider of player
+        playerMovement.gameObject.GetComponent<SpriteRenderer>().enabled = false; //turns off sprite of player
+        playerMovement.gameObject.GetComponent<Rigidbody2D>().isKinematic = true; //turns off rigidbody of player
+
+        playerMovement.gameObject.transform.position = currentVehicle.transform.position;
+        playerMovement.gameObject.transform.parent = currentVehicle.transform;
+
+        //update vehicle
+        currentVehicle.GetComponent<PlayerVehicleMovement>().enabled = true;
+        isInVehicle = true;
+    }
+
+    void exitCar()
+    {
+        //Update Player
+        playerMovement.enabled = true;
+        playerMovement.gameObject.GetComponent<Collider2D>().enabled = true;
+        playerMovement.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        playerMovement.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+
+        playerMovement.gameObject.transform.parent = null;
+
+        //Update Vehicle
+        currentVehicle.GetComponent<PlayerVehicleMovement>().enabled = false;
+        isInVehicle = false;
+    }
+}
---
+++ TopDownActionGame (CA2)/Assets/Scripts/Weapon.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int Magazine; //current number of bullets
+    public int maxMagazine; // max number of bullets that can be in the magazine
+
+    public int Reserves; //current reserves
+    public int maxReserves; //max number of bullets that can be in the reserves
+
+    public int ammoPerShot; //number of bullets used every time the weapon is fired
+    public bool isAutomatic; //is the weapon automatic or not?
+
+    public GameObject bulletPrefab; //bullet to instantiate
+    public Transform Spawn;
+
+    void Start()
+    {
+        Magazine = maxMagazine;
+        Reserves = maxReserves;
+    }
+
+    public bool hasAmmo()
+    {
+        //is the current number of bullets greater thanor equal to the amount of ammo used per shot
+        return Magazine >= ammoPerShot;
+    }
+
+    public void Reload()
+    {
+        if(Reserves >= maxMagazine)
+        {
+            int required = maxMagazine - Magazine; //determine the required number of bullets  
+
+            Magazine = maxMagazine; //fill magazine
+            Reserves -= maxMagazine; //update reserves
+        }
+        else //not enough in reserves to fully fill the magazine
+        {
+            Magazine = Reserves; //load whatever number we have
+            Reserves = 0;
+        }
+    }
+
+    //virtual indicates that this method can be extended in a child class
+    public virtual void Fire(Vector3 fireFromPosition) 
+    {
+        Magazine -= ammoPerShot;
+        if(Magazine <= 0)
+        {
+            Reload();
+        }
+    }
+}
